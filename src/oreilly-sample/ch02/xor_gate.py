f24297da2946a97fb3d583a5f3b4d5a8eca0d5cd
parmalatinter <parmalatinter@gmail.com>
2019-10-08 11:28:35 +0900

    ch02 learned

--- src/oreilly-sample/ch02/xor_gate.py
+++ src/oreilly-sample/ch02/xor_gate.py
@@ -1,4 +1,15 @@
 # coding: utf-8
+# 論理回路 2.2.1 ORゲート
+# パーセプトロンの動作原理式
+# {0 (w1x1 +w2x2 ≦ θ) *限界値を閾値θと呼ぶ 
+# {1 (w1x1 +w2x2 > θ)
+# *重みとバイアスによる方式
+# w1 や w2 は入力信号への重 要度をコントロールするパラメータ
+# bはニューロンの発火のしやすさ
+# {0 (b+w1x1 +w2x2 ≦ 0) θをb（バイアス）にする
+# {1 (b+w1x1 +w2x2 > 0)
+
+
 from and_gate import AND
 from or_gate import OR
 from nand_gate import NAND
--- src/test/exec.py
+++ src/test/exec.py
@@ -7,9 +7,22 @@ class Test(object):
     def send(self, title, message):
         print(title, message)
 
+def AND(x1, x2):
+    w1, w2, theta = 1, 1, 0
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+       return 0 
+    elif tmp > theta:
+       return 1
+
+
 def main():
-    test = Test()
-    test.send('title', 'message')
+    # test = Test()
+    # test.send('title', 'message')
+    print(AND(0, 0)) # 0 を出力
+    print(AND(1, 0)) # 0 を出力
+    print(AND(0, 1)) # 0 を出力
+    print(AND(1, 1)) # 1 を出力
 
 if __name__ == "__main__":
     main()
