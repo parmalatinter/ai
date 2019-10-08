234acad65dd4d0dae5e67524e417788a5b739a96
parmalatinter <parmalatinter@gmail.com>
2019-10-08 11:41:21 +0900

    ch02 learned

--- src/oreilly-sample/ch02/xor_gate.py
+++ src/oreilly-sample/ch02/xor_gate.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# 論理回路 2.2.1 ORゲート
+# 論理回路 2.2.1 XORゲート
 # パーセプトロンの動作原理式
 # {0 (w1x1 +w2x2 ≦ θ) *限界値を閾値θと呼ぶ 
 # {1 (w1x1 +w2x2 > θ)
@@ -8,8 +8,8 @@
 # bはニューロンの発火のしやすさ
 # {0 (b+w1x1 +w2x2 ≦ 0) θをb（バイアス）にする
 # {1 (b+w1x1 +w2x2 > 0)
-
-
+# NAND、ORの結果をANDに入力することでXORゲートが実現する
+# XORは2層のパーセプトロン
 from and_gate import AND
 from or_gate import OR
 from nand_gate import NAND
