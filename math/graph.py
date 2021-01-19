0929571ade4907b64e13d026e45ddb95a2b18342
Yosuke Koabayashi <parmalatinter@gmail.com>
2021-01-19 23:50:45 +0900

    add learn ai

---
+++ math/graph.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# グラフの表示
+# 参考　https://techacademy.jp/magazine/34230
+
+import sympy as sym
+from sympy.plotting import plot
+
+# %matplotlib inline
+
+# グラフ
+def Graph(y, tapple):
+    plot(y, tapple)
+
+def main():
+    # 変数として使用する文字を指定する
+    x, y = sym.symbols("x y")
+    y = x ** 2 + 2 * x - 3
+    Graph(y, (x, -10, 10))
+
+if __name__ == "__main__":
+    main()
