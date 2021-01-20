446089b346ed492290389e87015a8d798bcc5c1d
Yosuke Koabayashi <parmalatinter@gmail.com>
2021-01-20 23:06:13 +0900

    add Integral and Derivative

--- math/derivative.py
+++ math/derivative.py
@@ -16,22 +16,11 @@ def main():
     # y = x^2 + 2x - 3
     y = x ** 2 + 2 * x - 3
  
-    # 8 = x^2 - 12x 
-    eq = sym.Eq(x**2-12*x, -8)
-    # 方程式を解く
-    # [6 - 2*sqrt(7), 2*sqrt(7) + 6]
-    print(eq)
-    print(sym.solve(eq))
-
     # 微分
     y = sym.Derivative(y).doit()
     print(y)
     Graph(y, (x, -10, 10))
 
-    # 積分
-    y = sym.Integral(y).doit()
-    print(y)
-    Graph(y, (x, -10, 10))
 
 if __name__ == "__main__":
     main()
